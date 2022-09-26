31e23a9c377ff12379a3d57ff7be44847c45f3a3
Hugo <78080239+mordeur@users.noreply.github.com>
2022-09-26 16:20:46 +0200

    mise en place du début du carousel d'image

--- app/src/main/java/com/example/instantlike/MainActivity.java
+++ app/src/main/java/com/example/instantlike/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.instantlike;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -13,22 +15,29 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-//https://www.youtube.com/watch?v=8890GpBwn9w
- //https://www.youtube.com/watch?v=KY5vOVNqkGM
+// vidéo pour l'appareile photo : https://www.youtube.com/watch?v=8890GpBwn9w
+ // vidéo pour les liste view : https://www.youtube.com/watch?v=KY5vOVNqkGM
     private static final int RETOUR_PHOTO = 1;
-    ImageView imagePoster;
     Button adImage, poste;
     String photoPath = null;
     @Override
@@ -36,13 +45,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         iniActivity();
-
+        imageInfinie();
     }
+    private void imageInfinie(){
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        final ArrayList<String> imageList = new ArrayList<>();
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final ImageAdapter adapter = new ImageAdapter(imageList, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images");
+        progressBar.setVisibility(View.VISIBLE);
+        storageReference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference fileRef : listResult.getItems()) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            imageList.add(uri.toString());
+                            Log.d("item", uri.toString());
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            recyclerView.setAdapter(adapter);
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
     // initialistations du post
     private void iniActivity(){
         adImage = findViewById(R.id.adImageBtn);
         poste = findViewById(R.id.poste);
-        imagePoster = findViewById(R.id.imageEnvoiller);
         recupérationImage();
         adPoste();
     }
